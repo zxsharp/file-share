931d225c7400858ff8aef9d61c8b6ae3dac69ae9
zxsharp <ishanshdimaniya@gmail.com>
2025-09-09 02:48:14 +0530

    update cron and cleanup route

--- src/app/api/cleanup/route.ts
+++ src/app/api/cleanup/route.ts
@@ -1,7 +1,13 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { cleanupExpiredFiles } from '@/services/cleanup'
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  // auth token to only allow cron to trigger this end point
+  const auth = request.headers.get("authorization");
+  if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   console.log("cron ran.....");
   const count = await cleanupExpiredFiles()
   return NextResponse.json({ deleted: count }) 
