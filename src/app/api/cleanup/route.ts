a30bf3920dd18c9e1d98a2c90037ee864f18a66b
zxsharp <ishanshdimaniya@gmail.com>
2025-09-09 02:56:23 +0530

    use NextResponse for consistency

--- src/app/api/cleanup/route.ts
+++ src/app/api/cleanup/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
   // auth token to only allow cron to trigger this end point
   const auth = request.headers.get("authorization");
   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   console.log("cron ran.....");
