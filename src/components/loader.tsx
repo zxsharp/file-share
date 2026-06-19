3825a081358a4e1fd6aa1650eadcc19c4dd9db22
zxsharp <ishanshdimaniya@gmail.com>
2026-06-20 02:18:34 +0530

    add loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,9 @@
+import { LoaderOne } from "./ui/loader";
+
+export default function Loader() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <LoaderOne />
+        </div>
+    )
+}
